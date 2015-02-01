6bc0ec1648805b24f3f9ae99514d976794971373
wangshauilong <oopswangsl@gmail.com>
2015-02-01 13:41:36 +0800

    commit ex1 and ex2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your code later.
+# Anything after the # is ignored by python.
+print "I could have code like this." # and the comment here will be ignored.
+# you can also use a comment to "disable" or comment out a piece of code: # print "this won't be printed."
+print "This will run."
+
+# words hereafter are tested.
+print " if the words after # will be ignored by python?"
+# what will happen if the wolds are beyond single line, just like this ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
