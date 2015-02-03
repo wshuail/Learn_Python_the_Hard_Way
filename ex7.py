09421707cf5d9e7b661f9d3e58e070901560c3d9
wangshauilong <oopswangsl@gmail.com>
2015-02-04 00:16:45 +0800

    commit ex7-12

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print 'Mary had a little lamb.'
+print 'Its fleece was white as %s.' % 'snow'
+print 'And everywhere that Many went.'
+print '.' * 10 # what'd that do? I think it would print ten '.'.
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# Watch that comma at the end. Try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, # This will print Cheese
+print end7 + end8 + end9 + end10 + end11 + end12 # THis will print Burger
