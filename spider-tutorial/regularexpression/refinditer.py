9b71939497888c905ed98de6af238d220a7dcfd0
wangshuailong <oopswangsl@gmail.com>
2016-04-13 22:50:57 +0800

    change some files

--- spider-tutorial/regularexpression/refinditer.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-import re
-
-pattern = re.compile(r'\d+')
-
-for m in re.finditer(pattern, r'one1two2three3four4'):
-    print m.group()
--- spider-tutorial/spider_wanghai/urllib2_test02.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-import urllib2
-req = urllib2.Request('http://www.baidu.com/')
-response = urllib2.urlopen(req)
-the_page = response.read()
-print the_page
